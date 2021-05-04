13fbc0005a48494a0563799278b40f5a0aff7104
Suvankar Das <iambabai0@gmail.com>
2021-05-04 21:23:57 +0530

    Question Repository

---
+++ StackOverflow.Repositories/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using StackOverflow.DomainModels;
+
+namespace StackOverflow.Repositories.Interfaces
+{
+    public interface IQuestionRepository
+    {
+        void InsertQuestion(Question question);
+        void UpdateQuestionDetails(Question question);
+        /// <summary>
+        /// Based on the question id , update the votes of the particular question
+        /// </summary>
+        /// <param name="questionid"></param>
+        /// <param name="voteValue">Similar to stackoverflow downvote -1 , upvote +2 etc.</param>
+        void UpdateQuestionVotesCount(int questionid, int voteValue);
+
+        /// <summary>
+        /// If new Answer is added then return 1 and if deleted then return -1
+        /// </summary>
+        /// <param name="questionId"></param>
+        /// <param name="returnValue">1 for adding -1 for removing</param>
+        void UpdateQuestionAnswersCount(int questionId,int returnValue);
+
+        void UpdateQuestionViewsCount(int questionid);
+
+        void DeleteQuestion(int questionId);
+        List<Question> GetQuestions();
+        List<Question> GetQuestionsByCategoryId(int categoryId);
+        Question GetQuestionByQuestionId(int questionId);
+
+    }
+}
---
+++ StackOverflow.Repositories/Interfaces/QuestionRepository.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StackOverflow.DomainModels;
+
+namespace StackOverflow.Repositories.Interfaces
+{
+    public class QuestionRepository:IQuestionRepository
+    {
+        private readonly StackOverflowDbContext db;
+
+        public QuestionRepository()
+        {
+            db = new StackOverflowDbContext();
+        }
+        public void InsertQuestion(Question question)
+        {
+            db.Questions.Add(question);
+            db.SaveChanges();
+        }
+
+        public void UpdateQuestionDetails(Question question)
+        {
+            Question updatingQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == question.QuestionID);
+            if (updatingQuestion!=null)
+            {
+                updatingQuestion.QuestionName = question.QuestionName;
+                updatingQuestion.QuestionDateAndTime = question.QuestionDateAndTime;
+                updatingQuestion.CategoryID = question.CategoryID;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionVotesCount(int questionid, int voteValue)
+        {
+            Question updatingQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == questionid);
+            if (updatingQuestion != null)
+            {
+                updatingQuestion.VotesCount += voteValue;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionAnswersCount(int questionId, int returnValue)
+        {
+            Question updatingQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == questionId);
+            if (updatingQuestion != null)
+            {
+                updatingQuestion.AnswersCount += returnValue;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionViewsCount(int questionid)
+        {
+            Question updatingQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == questionid);
+            if (updatingQuestion != null)
+            {
+                updatingQuestion.ViewsCount += 1;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteQuestion(int questionId)
+        {
+            Question deletingQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == questionId);
+            if (deletingQuestion != null)
+            {
+                db.Questions.Remove(deletingQuestion);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Question> GetQuestions()
+        {
+            return db.Questions.OrderByDescending(q => q.QuestionDateAndTime).ToList();
+        }
+
+        public List<Question> GetQuestionsByCategoryId(int categoryId)
+        {
+            List<Question> questions = db.Questions.Where(q => q.CategoryID == categoryId).ToList();
+            return questions;
+        }
+
+        public Question GetQuestionByQuestionId(int questionId)
+        {
+            Question fetchQuestion = db.Questions.FirstOrDefault(q => q.QuestionID == questionId);
+            return fetchQuestion;
+        }
+    }
+}
