8d01c581e7cfc979994a502679703b9547a5827d
Suvankar Das <iambabai0@gmail.com>
2021-05-04 11:13:39 +0530

    Questio domain model

---
+++ StackOverflow.DomainModels/Question.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.DomainModels
+{
+    public class Question
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionID { get; set; }
+
+        public string QuestionName { get; set; }
+        public DateTime QuestionDateAndTime { get; set; }
+
+        public int UserID { get; set; }
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+
+        public int VotesCount { get; set; }
+        public int AnswersCount  { get; set; }
+        public int ViewsCount { get; set; }
+
+        public virtual IList<Answer> Answers { get; set; }
+
+    }
+}
