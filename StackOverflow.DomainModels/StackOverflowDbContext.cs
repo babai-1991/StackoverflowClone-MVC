54ab5424274c28d7a86610c1713117a0ca43cd16
Suvankar Das <iambabai0@gmail.com>
2021-05-04 11:57:18 +0530

    Adding connection string and DbContext under domain model

---
+++ StackOverflow.DomainModels/StackOverflowDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.DomainModels
+{
+    class StackOverflowDbContext:DbContext
+    {
+        public StackOverflowDbContext():base("StackOverflowDatabaseDbContext")
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+    }
+}
