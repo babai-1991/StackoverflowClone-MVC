31c55715a95df5a82b9cbe82e4a672b7dcbe9ff1
Suvankar Das <iambabai0@gmail.com>
2021-05-05 11:25:39 +0530

    Edit answer view model

---
+++ StackOverflow.ViewModels/EditAnswerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class EditAnswerViewModel
+    {
+        [Required]
+        public int AnswerId { get; set; }
+        [Required]
+        public int AnswerText { get; set; }
+        [Required]
+        public DateTime AnswerDateTime { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int QuestionId { get; set; }
+        [Required]
+        public int VotesCount { get; set; }
+        [Required]
+        public virtual QuestionViewModel Question { get; set; }
+
+    }
+}
