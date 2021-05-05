fe34afae6f52cfcce957b7f3f9b917f441190998
Suvankar Das <iambabai0@gmail.com>
2021-05-05 10:21:00 +0530

    EditUserPasswordViewModel

---
+++ StackOverflow.ViewModels/EditUserPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class EditUserPasswordViewModel
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
