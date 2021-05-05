3fd5925e4691ccbbbcf5e8d0710e9af1a9fd3634
Suvankar Das <iambabai0@gmail.com>
2021-05-05 09:55:50 +0530

    RegisterViewModel

---
+++ StackOverflow.ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression("/^[a-zA-Z]*$/")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"/^\S+@\S+\.\S+$/")]
+        public string  Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]*$")]
+        public string Mobile { get; set; }
+    }
+}
