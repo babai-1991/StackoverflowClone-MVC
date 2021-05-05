c7d7dc9b2407419531e98671554c25d2bb21ef15
Suvankar Das <iambabai0@gmail.com>
2021-05-05 11:05:22 +0530

    Edit question view model

---
+++ StackOverflow.ViewModels/EditQuestionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class EditQuestionViewModel
+    {
+        [Required]
+        public int QuestionId { get; set; }
+        [Required]
+        public string QuestionName { get; set; }
+        [Required]
+        public DateTime QuestionDateTime { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
