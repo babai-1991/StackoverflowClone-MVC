88c5150168125588d8c0ee7c6d0462fff7a90a22
Suvankar Das <iambabai0@gmail.com>
2021-05-05 11:28:12 +0530

    Vote ViewModel

--- StackOverflow.ViewModels/VoteViewModel.cs
+++ StackOverflow.ViewModels/VoteViewModel.cs
@@ -2,6 +2,9 @@
 {
     public class VoteViewModel
     {
-
+        public int VoteId { get; set; }
+        public int UserId { get; set; }
+        public int AnswerId { get; set; }
+        public int VoteValue { get; set; }
     }
 }
