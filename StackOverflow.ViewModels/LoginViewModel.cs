7e28853c8135c064c0b1844f696b019525247e91
Suvankar Das <iambabai0@gmail.com>
2021-05-05 10:04:55 +0530

    Login View Model

---
+++ StackOverflow.ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [RegularExpression(@"/^\S+@\S+\.\S+$/")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
