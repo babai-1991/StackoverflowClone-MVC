92f03607d09c5d5b5835db8b8b7ca1839d0ffc80
Suvankar Das <iambabai0@gmail.com>
2021-05-05 10:54:22 +0530

    New Question View Model

---
+++ StackOverflow.ViewModels/NewQuestionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class NewQuestionViewModel
+    {
+        [Required]
+        public string QuestionName { get; set; }
+        [Required]
+        public DateTime QuestionDateTime { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int VotesCount { get; set; }
+        [Required]
+        public int AnswersCount { get; set; }
+        [Required]
+        public int ViewsCount { get; set; }
+
+    }
+}
