7fd3dc2d36ea25162503601921bc9d276aa6bc51
Suvankar Das <iambabai0@gmail.com>
2021-05-05 10:50:15 +0530

    Category View Model

---
+++ StackOverflow.ViewModels/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
