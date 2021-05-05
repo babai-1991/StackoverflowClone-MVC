3f0b54b7195a7ebaeb7643fd4d52f0b42edb46fc
Suvankar Das <iambabai0@gmail.com>
2021-05-05 10:17:03 +0530

    EditUserDetailsViewModel

---
+++ StackOverflow.ViewModels/EditUserDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels
+{
+    public class EditUserDetailsViewModel
+    {
+        [Required]
+        [RegularExpression("/^[a-zA-Z]*$/")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"/^\S+@\S+\.\S+$/")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]*$")]
+        public string Mobile { get; set; }
+    }
+}
