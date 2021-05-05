57aa87b917c841d4cde45316b4c42d3fd3ca7def
Suvankar Das <iambabai0@gmail.com>
2021-05-05 20:22:14 +0530

    Extend Automapper in ServiceLayer

---
+++ StackOverflow.ServiceLayer/MapperExtension.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+
+namespace StackOverflow.ServiceLayer
+{
+    public static class MapperExtension
+    {
+        private static void IgnoreUnMapped(this IProfileExpression profile)
+        {
+            profile.ForAllMaps(IgnoreUnMappedProperties);
+        }
+
+        private static void IgnoreUnMappedProperties(TypeMap map, IMappingExpression expression)
+        {
+            foreach (string unmappedProperty in map.GetUnmappedPropertyNames())
+            {
+                if (map.SourceType.GetProperty(unmappedProperty) != null)
+                {
+                    expression.ForSourceMember(unmappedProperty, option => option.Ignore());
+                }
+                if (map.DestinationType.GetProperty(unmappedProperty) != null)
+                {
+                    expression.ForSourceMember(unmappedProperty, option => option.Ignore());
+                }
+            }
+        }
+    }
+}
