fdb625f7bd5bb3c5c561dc81e4b541580fe83e7c
Suvankar Das <iambabai0@gmail.com>
2021-05-06 13:16:02 +0530

    CategoryService

---
+++ StackOverflow.ServiceLayer/CategoryService.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using StackOverflow.DomainModels;
+using StackOverflow.Repositories;
+using StackOverflow.Repositories.Interfaces;
+using StackOverflow.ServiceLayer.Interfaces;
+using StackOverflow.ViewModels;
+
+namespace StackOverflow.ServiceLayer
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoryService()
+        {
+            _categoriesRepository = new CategoriesRepository();
+        }
+        public void InsertCategory(CategoryViewModel categoryViewModel)
+        {
+            var configuration = new MapperConfiguration(config =>
+           {
+               config.CreateMap<CategoryViewModel, Category>();
+               config.IgnoreUnMapped();
+           });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            Category category = mapper.Map<CategoryViewModel, Category>(categoryViewModel);
+            _categoriesRepository.InsertCategory(category);
+
+        }
+
+        public void UpdateCategory(CategoryViewModel categoryViewModel)
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CategoryViewModel, Category>();
+                config.IgnoreUnMapped();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            Category category = mapper.Map<CategoryViewModel, Category>(categoryViewModel);
+            _categoriesRepository.UpdateCategory(category);
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            _categoriesRepository.DeleteCategory(categoryId);
+        }
+
+        public List<CategoryViewModel> GetCategories()
+        {
+            List<Category> categories = _categoriesRepository.GetCategories();
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Category, CategoryViewModel>();
+                config.IgnoreUnMapped();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            List<CategoryViewModel> categoryViewModels = mapper.Map<List<Category>, List<CategoryViewModel>>(categories);
+            return categoryViewModels;
+        }
+
+        public List<CategoryViewModel> GetCategoryByCategoryId(int categoryid)
+        {
+            List<Category> categories = _categoriesRepository.GetCategoryByCategoryId(categoryid);
+            List<CategoryViewModel> categoryViewModel = null;
+            if (categories != null)
+            {
+                var configuration = new MapperConfiguration(config =>
+                {
+                    config.CreateMap<Category, CategoryViewModel>();
+                    config.IgnoreUnMapped();
+                });
+                IMapper mapper = configuration.CreateMapper();
+
+                categoryViewModel = mapper.Map<List<Category>, List<CategoryViewModel>>(categories);
+            }
+
+            return categoryViewModel;
+        }
+    }
+}
---
+++ StackOverflow.ServiceLayer/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflow.ViewModels;
+
+namespace StackOverflow.ServiceLayer.Interfaces
+{
+    public interface ICategoryService
+    {
+        void InsertCategory(CategoryViewModel categoryViewModel);
+        void UpdateCategory(CategoryViewModel categoryViewModel);
+        void DeleteCategory(int categoryId);
+        List<CategoryViewModel> GetCategories();
+        List<CategoryViewModel> GetCategoryByCategoryId(int categoryid);
+    }
+}
