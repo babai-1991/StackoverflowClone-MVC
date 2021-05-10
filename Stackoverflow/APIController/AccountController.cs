eac6494c979526beda51149205c5005837b293a5
Suvankar Das <iambabai0@gmail.com>
2021-05-10 20:36:20 +0530

    Checking duplication of email

---
+++ Stackoverflow/APIController/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StackOverflow.ServiceLayer.Interfaces;
+using StackOverflow.ViewModels;
+
+namespace Stackoverflow.APIController
+{
+    public class AccountController : ApiController
+    {
+        private readonly IUsersService _usersService;
+
+        public AccountController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        
+        public string Get(string email)
+        {
+            UserViewModel model = _usersService.GetUserByEmail(email);
+            if (model!=null)
+            {
+                return "Found";
+            }
+            else
+            {
+                return "Not Found";
+            }
+        }
+    }
+}
